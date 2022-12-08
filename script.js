5a8edff5f66667a6e63a5feb597d2e4032ae6faa
WHL99 <xjv298347@gmail.com>
2022-12-08 15:17:28 +0100

    Implement responsive web design.

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-$(document).ready(function () {
-    $(".post-wrapper").slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        nextArrow: $(".next"),
-        prevArrow: $(".prev"),
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+$(".post-wrapper").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
             }
-        ]
-    });
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
 
 function queryFetch(query) {
@@ -43,7 +41,6 @@ function queryFetch(query) {
     })
         .then(res => res.json())
         .then(data => {
-
             data.data.products.products.forEach((product) => {
 
                 const outsideDiv = document.getElementById('post-wrapper')
