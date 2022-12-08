ee88dd625a33729cee6c0ce6e6db9939df0ffecb
WHL99 <xjv298347@gmail.com>
2022-12-08 14:12:34 +0100

    Create a carousel. Implement products’ names should not break into two lines, remove trailing decimal zeros, re-direct the user to the product page, and basic style.

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+$(document).ready(function () {
+    $(".post-wrapper").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        nextArrow: $(".next"),
+        prevArrow: $(".prev"),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
 function queryFetch(query) {
     return (fetch('https://api.nosto.com/v1/graphql', {
         method: 'POST',
@@ -9,14 +43,53 @@ function queryFetch(query) {
     })
         .then(res => res.json())
         .then(data => {
-            console.log(data.data.products.products)
+
+            data.data.products.products.forEach((product) => {
+
+                const outsideDiv = document.getElementById('post-wrapper')
+
+                const divMD = document.createElement('div')
+                const divForInfo = document.createElement('div')
+                const aLink = document.createElement('a')
+                const image = document.createElement('img')
+                const title = document.createElement('h3')
+                const subTitle = document.createElement('h5')
+                const price = document.createElement('p')
+
+                title.className = 'font'
+                subTitle.className = 'font'
+                price.className = 'font'
+                aLink.href = product.url
+                divMD.className = 'post'
+                divForInfo.className = 'post-info'
+                image.className = 'slider-image'
+                image.loading = 'lazy'
+                image.src = product.imageUrl
+                image.alt = 'product-photo'
+
+                title.innerText = product.brand
+                subTitle.innerText = product.name
+                price.innerText = `€ ${priceWithoutZeros(product.price, 2)}`
+
+                divMD.append(aLink)
+                aLink.append(image)
+                divForInfo.append(title)
+                divForInfo.append(subTitle)
+                divForInfo.append(price)
+                outsideDiv.append(divMD)
+                divMD.append(divForInfo)
+            })
         })
     )
 }
 
+function priceWithoutZeros(num, precision) {
+    return num.toFixed(precision).replace(/\.0+$/, '')
+}
+
 queryFetch(`
 query {
-    products(limit: 50) {
+    products(limit: 5) {
         products {
             name
             price
